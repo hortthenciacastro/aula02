57223c1caf63620d0cc73f97f50166125d54bc11
hortthenciacastro <159339706+hortthenciacastro@users.noreply.github.com>
2024-10-21 09:14:11 -0300

    Update index.jsx

--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [DadosUsuario,setDadosUsuario] = useState([]);
 
   useEffect(() => { 
 
-    const  ????  = async () => {
+    const  buscardados  = async () => {
         const resposta = await fetch('https://randomuser.me/api');
         const dados = await resposta.json();
-         //complete o código
+        setDadosUsuario(dados); //complete o código
     }
-    //complete o código
+   buscardados(); //complete o código
   }, []);
 
   return (
     <>
       <h1>Usuário</h1>
       <ul>
-        {
+        {DadosUsuario.map(dados =>(
+          <li key={dados.id}>
+          <h2>{dados.title}</h2>
+          <p>{dados.description}</p>
+          </li>
+          ))
             //complete o código
         }
       </ul>
